bd6c29d412d7b84c75544793d43e2fc1f64b00af
edwin <edwinkun@gmail.com>
2023-03-15 15:58:04 +0700

    initial commit :spider:

---
+++ src/main/java/com/edw/model/Loan.java
@@ -0,0 +1,50 @@
+package com.edw.model;
+
+/**
+ * <pre>
+ *     com.edw.model.Loan
+ * </pre>
+ *
+ * @author Muhammad Edwin < edwin at redhat dot com >
+ * 15 Mar 2023 10:35
+ */
+public class Loan {
+
+    private Integer age;
+    private Double salary;
+
+    private String risk;
+
+    public Loan() {
+    }
+
+    public Loan(Integer age, Double salary, String risk) {
+        this.age = age;
+        this.salary = salary;
+        this.risk = risk;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getRisk() {
+        return risk;
+    }
+
+    public void setRisk(String risk) {
+        this.risk = risk;
+    }
+}
